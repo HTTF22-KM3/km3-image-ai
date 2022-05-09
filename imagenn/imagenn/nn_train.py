b9c2a1d7880456af3aa21c0457c26e7aefc46790
merlinroser <mrr-py@outlook.com>
2022-05-09 20:56:45 +0200

    Added Discriminative AI, and it's helper function, as well as the Generative AI and its helper function. It will probably need A LOT of work before it can be executed, because I still don't fully understand how this works.

--- imagenn/imagenn/nn_train.py
+++ imagenn/imagenn/nn_train.py
@@ -70,3 +70,67 @@ def conv(in_channels, out_channels, kernel_size, stride=2, padding=1, batch_norm
         layers.append(nn.BatchNorm2d(out_channels))
 
     return nn.Sequential(*layers)
+
+
+# TODO: Get behind the math of this algorithm
+# TODO: Read paper for Discriminative AI and Generative AI
+class Discriminator():
+    def __init__(self, conv_dim):
+        super(Discriminator, self).__init__()
+        self.conv_dim = conv_dim
+
+        self.cv1 = conv(3, self.conv_dim, 4, batch_norm=False)
+        self.cv2 = conv(self.conv_dim, self.conv_dim*2, 4, batch_norm=True)
+        self.cv3 = conv(self.conv_dim*2, self.conv_dim*4, batch_norm=True)
+        self.cv4 = conv(self.conv_dim * 4, self.conv_dim * 8, batch_norm=True)
+
+        self.fc1 = nn.Linear(self.conv_dim*8*2*2, 1)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.cv1(x), 0.2)
+        x = F.leaky_relu(self.cv2(x), 0.2)
+        x = F.leaky_relu(self.cv3(x), 0.2)
+        x = F.leaky_relu(self.cv4(x), 0.2)
+
+        x = x.view(-1, self.conv_dim*8*2*2)
+        x = self.fc1(x)
+        return x
+
+
+# Helper function for generative AI
+def deconv(in_channels, out_channels, kernel_size, stride = 2, padding = 1, batch_norm = True):
+    layers = []
+    convt_layer = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+
+    layers.append(convt_layer)
+
+    if batch_norm:
+        layers.append(nn.BatchNorm2d(out_channels))
+
+    return nn.Sequential(*layers)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_size, conv_dim):
+        super(Generator, self).__init__()
+
+        self.z_size = z_size
+        self.conv_dim = conv_dim
+
+        # Declaring the fully connected layer
+        self.fc = nn.Linear(z_size, self.conv_dim*8*2*2)
+        self.dcv1 = deconv(self.conv_dim*8, self.conv_dim*4, 4, batch_norm=True)
+        self.dcv2 = deconv(self.conv_dim*4, self.conv_dim*2, 4, batch_norm=True)
+        self.dcv3 = deconv(self.conv_dim*2, self.conv_dim, 4, batch_norm=True)
+        self.dcv4 = deconv(self.conv_dim, 3, 4, batch_norm=False)
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, self.conv_dim*8, 2, 2)
+
+        x = F.relu(self.dcv1(x))
+        x = F.relu(self.dcv2(x))
+        x = F.relu(self.dcv3(x))
+        x = F.relu(self.dcv4(x))
+
+        return x
